ecfa6cbdc13f0495dc1a26ab06d370a840c52fab
Saumya Jain <samjain0913@gmail.com>
2020-07-14 14:22:20 +0530

    changes

---
+++ android/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+h
+8app/src/main/kotlin/com/oblivion/barcode/MainActivity.kt,f\c\fc1a3cddf91a9520b20a968ae6791872c801e975
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
--- lib/main.dart
+++ lib/main.dart
@@ -35,30 +35,81 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (_isExisted) {
+      return new Material(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              new Text('QR Reader'),
+              new TextField(
+                decoration: InputDecoration(
+                  labelText: "Please input the image path",
+                ),
+                controller: myController,
+                autofocus: true,
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new RaisedButton(
+                    child: new Text('Read'),
+                      onPressed: _showRead,
+                  ),
+                  new RaisedButton(
+                    child: new Text('Reset'),
+                      onPressed: _resetResult
+                  ),
+                  new RaisedButton(
+                    child: new Text("Test"),
+                    onPressed: _showNativeView,
+                  ),
+                ],
+              ),
+              new Image.file(new File(_filename)),
+              new Text('$_result'),
+            ],
+          )
+        )
+      );
+    } else {
+      return new Material(
+        child: new Center(
+          child: new Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Text('QR Reader'),
+                new TextField(
+                  decoration: InputDecoration(
+                    labelText: "Please input the image path",
+                  ),
+                  controller: myController,
+                  autofocus: true,
+                ),
+                new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new RaisedButton(
+                          child: new Text('Read'),
+                          onPressed: _showRead
+                      ),
+                      new RaisedButton(
+                          child: new Text('Reset'),
+                          onPressed: _resetResult
+                      ),
+                      new RaisedButton(
+                        child: new Text("Test"),
+                        onPressed: _showNativeView,
+                      ),
+                    ]
+                ),
+                new Text('$_result'),
+            ]
+          )
+        )
+      );
+    }
 
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new RaisedButton(
-              child: new Text('Move to Native World!'),
-              onPressed: _showNativeView,
-            ),
-            new RaisedButton(
-              child: new Text('Read'),
-              onPressed: _showRead,
-            ),
-            new TextField(
-              controller: myController,
-            )
-          ],
-        ),
-      ),
-    );
   }
 
   Future<Null> _showNativeView() async {
@@ -67,7 +118,10 @@ class MyHomePage extends StatelessWidget {
 
   Future<Null> _showRead() async {
     String message = myController.text;
+    _filename = message;
     await platform.invokeMethod(KEY_READ,{"barcode":message});
+    if (!mounted)
+      return;
   }
 
   Future<dynamic> _handleMethod(MethodCall call) async {
